8d958194f4e06bb13f77cb9736a910f74cdcf266
soponalways <soponalways@gmail.com>
2025-09-07 19:17:57 +0600

    fix simple toast related issue

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -3,7 +3,7 @@ import { signIn } from "next-auth/react";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import { useState } from "react";
-import { toast } from "react-toastify";
+import { toast } from "react-hot-toast";
 
 export default function RegisterPage() {
     const [form, setForm] = useState({ name: "", email: "", password: "" });
